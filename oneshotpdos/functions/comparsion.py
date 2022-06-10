d4e43437eff8346746e0e2b2f92e0e1f9142a098
Kazukifujii <fuziikazuki@gmail.com>
2022-06-10 18:15:28 +0900

    add 'differance' method in ComparsionPdos class

--- oneshotpdos/functions/comparsion.py
+++ oneshotpdos/functions/comparsion.py
@@ -1,7 +1,9 @@
+from importlib.util import spec_from_file_location
 import pandas as pd
-from readinfo import set_pdosdata
+from pdosfilter import PdosFilter
+from readinfo import setcifdata
 import re,os,itertools
-
+from constant import element_group,ORBITAL
 from makepdos import listup_cif
 
 def cif_conbination(cif_dir):
@@ -12,25 +14,47 @@ def cif_conbination(cif_dir):
             ciflist=[re.search(r"([^/]*?)$",s.strip()).group() for s in f.readlines()]
     return list(itertools.combinations(ciflist,2))
 
-class comparsion_pdos_in_directory():
+class ComparsionPdos():
     """
-    import re
-    directory='/home/fujikazuki/cif_pdos/main/cif_list/result/'
-    cif_list=cif_conbination('/home/fujikazuki/cif_pdos/main/cif_list.txt') #return tuple in list
-    conp=comparsion_pdos_in_directory(*cif_list[0],directory)
-    print(conp.comp_dire_1)
-    for s in conp.pdos_data_1:
-        print(s)
-        print(conp.pdos_data_1[s].shape)
-    print(conp.comp_dire_2)
-    for s in conp.pdos_data_2:
-        print(s)
-        print(conp.pdos_data_2[s].shape)
     """
-    def __init__(self,comp_dire1,comp_dire2,directory):
-        self.comp_dire_1=directory+'/'+comp_dire1.replace('.cif','')
-        self.comp_dire_2=directory+'/'+comp_dire2.replace('.cif','')
-        self.pdos_data_1=set_pdosdata(self.comp_dire_1)
-        self.pdos_data_2=set_pdosdata(self.comp_dire_2)
+    def __init__(self,cifadress_1,cifadress_2):
+        self.pdos_data_1=PdosFilter(cifadress_1)
+        self.pdos_data_2=PdosFilter(cifadress_2)
+        if len(self.pdos_data_1.cifdata.specsite)!=len(self.pdos_data_2.cifdata.specsite):
+            print('not same number of specsite')
+            return
         self.weight=dict()
-
+        self.speclenge=len(self.pdos_data_1.cifdata.specsite)
+        atomlist1=[(str(i[1]),element_group[i[1][0]]) for i in self.pdos_data_1.cifdata.specsite]
+        atomlist2=[(str(i[1]),element_group[i[1][0]]) for i in self.pdos_data_2.cifdata.specsite]
+        atomlist1.sort(key = lambda x: x[1])
+        atomlist2.sort(key = lambda x: x[1])
+        self.atomlist1=[s for s,_ in atomlist1]
+        self.atomlist2=[s for s,_ in atomlist2]
+    
+    def gaussian(self,Sigma):
+        self.pdos_data_1.gaussianfilter(sigma=Sigma)
+        self.pdos_data_2.gaussianfilter(sigma=Sigma)
+        
+    
+    def difference(self):
+        #sort by element group number
+        if len(self.pdos_data_1.cifdata.specsite)!=len(self.pdos_data_2.cifdata.specsite):
+            print('not same number of specsite')
+            return
+        
+        difdict=dict()
+        import matplotlib.pyplot as plt
+        for orbital in ORBITAL:
+            difdf=pd.DataFrame()
+            for j in range(self.speclenge):
+                difdf[j]=self.pdos_data_1.sameorbital_pdos[orbital][self.atomlist1[j]]-self.pdos_data_2.sameorbital_pdos[orbital][self.atomlist2[j]]
+            self.ifdict[orbital]=difdf
+dir='/home/fujikazuki/gaustest'
+resultdir='/home/fujikazuki/gaustest/classtest'
+ciflist=[s.replace('\n','')  for s in open(dir+'/cif_list.txt')]
+tc=ComparsionPdos(cifadress_1=ciflist[0],cifadress_2=ciflist[1])
+print(tc.pdos_data_1.cifdata.cifnumber)
+print(tc.pdos_data_2.cifdata.cifnumber)
+#tc.gaussian(Sigma=0.5)
+tc.difference()
